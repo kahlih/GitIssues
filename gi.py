c51953f7d4a51a948ed15e1f6c2057909e34ece0
Kahli Marie Holmes <kahlimarieholmes@MacBook-Pro.local>
2015-09-12 12:45:45 -0500

    added listing and opening issue capability

--- gi.py
+++ gi.py
@@ -1,20 +1,48 @@
 import click
+import requests
+import json
+import getpass
 
-username = ''
-repo = ''
+username = 'kahlih'
+repo = 'Competitive-Programmming'
 
-@click.command()
-@click.option('--config', nargs=2,type=str, help="This command lists the issues in your repo")
-def cli(config):
-	username = config[0]
-	repo = config[1]
-	target = open(".config", 'w').close()	
-	target = open(".config", 'w')
-	target.write(username)
-	target.write("\n")
-	target.write(repo)
-	target.write("\n")	
-	print("Hello!", username, repo)
-	print("username", username)
-	print("repo ", repo)
+url_head = "https://api.github.com/repos/"
+
+@click.group()
+def cli():
+	pass
+
+@cli.command()
+def list():
+	url = url_head + username + "/" + repo + "/issues"
+	response = requests.get("https://api.github.com/repos/kahlih/Competitive-Programming/issues")	
+	d=json.loads(response.content.decode('utf8'))
+	#d = d[0]
+	click.echo(click.style("Issues for Repo: Competitive-Programming", fg="green", reverse=True))	
+	for i in d:
+		s = str("#" + str(i["number"]) + ": " + i["title"])
+		click.echo(click.style(s, fg="blue"))
+
+@cli.command()
+@click.option("--t", default="Another Issue", help="none") 
+@click.argument('message')
+def open(t,message):
+	username = input('Github username: ')
+	password = getpass.getpass('Github password: ')	
+	
+	d = {}
+	d["title"]=t
+	d["body"]=message
+	
+	this_url = url_head + username + "/" + repo + "/issues"
+	res = requests.post(
+        	url="https://api.github.com/repos/kahlih/Competitive-Programming/issues",
+        	auth = (username, password),
+        	data = json.dumps(d),
+        )
+	if(res.status_code == 201):
+		click.echo(click.style("Issue created", fg="cyan")) 
+	else:
+		print(res.status_code, res.reason)
+
 
--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
 	py_modules = ['gi'],
 	install_requires=[
 		'Click',
+		'requests'
 	],
 	entry_points=""" 
 		[console_scripts]
